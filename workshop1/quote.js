9dd5df659a07784a7f61d1bb904141ccba3401af
zano16 <zn3052@bard.edu>
2016-08-16 15:09:47 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -4,8 +4,8 @@
  * L&T quote generator
  */
 
-var things = ["Music", "Art", "Dance", "Theater", "Math", "Architecture"];
-var programmers = ["musician", "artist", "choreographer", "director", "mathematician", "architect"]
+var things = ["Talking", "Sleeping", "Laughing", "Writing", "Eating", "Working"];
+var programmers = ["Tlker", "Sleeper", "Laugher", "Writer", "Eater", "Worker"]
 var castles = ["museums", "bridges", "cities"]
 var exertions = ["effort", "strain", "struggle", "toil", "endeavor", "hard work", "labor"];
 var airs = ["sky", "atmosphere", "heavens", "ether", "wind"];
